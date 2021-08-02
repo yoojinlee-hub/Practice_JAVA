558be6c209959b3ce01b3c43becc72d60da75013
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-08-02 10:53:22 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+/*comment*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+/* class 객체지향 프로그래밍의 기본  단위
+* class HelloWorld : HelloWorld라는 이름의 클래스를 만든다는 의미
+* */
+/* public : 누구나 사용할 수 있는 공적인 클래스
+* 두번째 줄은 HelloWorld 클래스 안에 위치함 - 클래스가 어떤 동작을 할 지 정의해줌. //메소드(클래스 안에 정의된 함수)
+* static : 그 부분을 바로 실행 가능하게 만들어 줌 . main은 가장 첫번째로 실행되어야 하기에 main 앞에는 항상 static
+* String[] args : 메소드에 전달되는 값, arges는 변수이름
+* System.in, System.out, System.err
+* println() : 출력하다 // 메소드
+* " " : 문자열
+* */
+
+/*
+* public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성하세요
+        System.out.println("Hello Codeit!");
+    }
+}
+* */
