b436d09f0bad5440677b3a27e9119cf7253a798c
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-08-03 09:15:01 +0900

    Add files via upload

---
+++ self_introduction.java
@@ -0,0 +1,50 @@
+public class self_introduction {
+    public static void main(String[] args) {
+        // 변수 정의 (자료형 잘 선정해서)
+        // 코드를 입력하세요.
+        String codeit;
+        String name;
+        String birthday;
+        String marriage;
+        String semi;
+        String y;
+        String m;
+        String d;
+        int year;
+        int month;
+        int day;
+        boolean myBoolean;
+        // 값 대입
+        // 코드를 입력하세요.
+        codeit = "코드잇";
+        name = "이름";
+        birthday="생일";
+        marriage = "결혼";
+        semi = ": ";
+        year = 2015 ;
+        month = 7 ;
+        day = 20 ;
+        myBoolean = false ;
+        y = "년 ";
+        m ="월 ";
+        d ="일 ";
+        // 자기 소개
+        // 코드를 입력하세요.
+        System.out.print(name);
+        System.out.print(semi);
+        System.out.println(codeit);
+
+        System.out.print(birthday);
+        System.out.print(semi);
+        System.out.print(year);
+        System.out.print(y);
+        System.out.print(month);
+        System.out.print(m);
+        System.out.print(day);
+        System.out.println(d);
+
+        System.out.print(marriage);
+        System.out.print(semi);
+        System.out.print(myBoolean);
+    }
+}
