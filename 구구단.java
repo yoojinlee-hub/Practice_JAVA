2ebe939db26dcd5819f6e1257e4a1f7a45507ce5
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-08-04 18:03:28 +0900

    Add files via upload

---
+++ 구구단.java
@@ -0,0 +1,10 @@
+public class 구구단 {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성하세요.
+        for(int i=1; i<=9 ; i++){
+            for(int j=1 ; j<=9; j++){
+                System.out.println(i + "*" + j + "=" + (i*j));
+            }
+        }
+    }
+}
