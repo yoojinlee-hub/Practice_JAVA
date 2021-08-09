d108c796df83bf109aeb4986d44d78b1aba6222c
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-08-09 19:54:39 +0900

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,24 @@
+public class BankAccount {
+    int balance;
+    Person owner;
+
+    //파라미터 : 입금할 액수(정수형)
+    //리턴: 성공여부(불린)
+    //boolean deposit(int amount){
+//
+  //  }
+
+    //파라미터 : 출금할 액수(정수)
+    //리턴 : 성공여부(불린)
+  //  boolean withdrow(int amount){
+//
+ //   }
+
+    //첫번째 파라미터: 받는 사람(Person)
+    //두번째 파라미터: 이체할 금액(정수)
+    //리턴: 성공여부(불린)
+ //   boolean transfer(Person to, int amount){
+//
+  //  }
+
+}
---
+++ practice_class.java
@@ -0,0 +1,21 @@
+public class practice_class { //객체의 설계도 main 메소드 BankDriver
+    public static void main(String[] args){
+        // 사람 선언
+        Person p1 = new Person();
+        p1.name = "김유진";
+        p1.age = 26;
+        p1.cashAmount = 30000;
+
+        // 은행 계좌 생성
+        BankAccount a1 = new BankAccount();
+        a1.balance = 100000;
+
+        p1.account = a1;
+        a1.owner = p1;
+
+        //test
+        System.out.println(p1.account.balance);
+        System.out.println(a1.owner.name);
+
+    }
+}
