47d484424597b35e04a5a3e8e20c003b0a1b7a31
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-08-10 13:02:02 +0900

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,6 +1,30 @@
 public class Person {
-    String name;
-    int age;
-    int cashAmount;
-    BankAccount account;
+    private String name;
+    private int age; //접근제어자
+    private int cashAmount;
+    private BankAccount account;
+
+    public void setAge(int newAge){
+        if (newAge>=0) {
+            age = newAge;
+        }
+    }
+    public int getAge(){
+            return age;
+        }
+
+    public void setName(String newName){
+            name = newName;
+        }
+    public String getName(){
+        return name;
+    }
+     public void setCashAmount(int newCashAmount){
+            if(newCashAmount>=0){
+                cashAmount = newCashAmount;
+            }
+        }
+     public int getCashAmount(){
+            return cashAmount;
+        }
 }
--- practice_class.java
+++ practice_class.java
@@ -2,10 +2,9 @@ public class practice_class { //객체의 설계도 main 메소드 BankDriver
     public static void main(String[] args){
         // 사람 선언
         Person p1 = new Person();
-        p1.name = "김유진";
-        p1.age = 26;
+        //p1.name = "김유진";
+        p1.setAge(26);
         p1.cashAmount = 30000;
-
         // 은행 계좌 생성
         BankAccount a1 = new BankAccount();
         a1.balance = 100000;
@@ -16,6 +15,6 @@ public class practice_class { //객체의 설계도 main 메소드 BankDriver
         //test
         System.out.println(p1.account.balance);
         System.out.println(a1.owner.name);
-
+        System.out.println(p1.getAge);
     }
 }
