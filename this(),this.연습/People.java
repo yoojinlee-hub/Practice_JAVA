d89534ba5fd82cc15d67bea049446aaa2e9c9baf
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-10-08 15:35:31 +0900

    Add files via upload

---
+++ this(),this.연습/People.java
@@ -0,0 +1,12 @@
+public class People {
+    String name;
+    int age;
+
+    People(){
+        this("이름없음",31);
+    }
+    People(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ this(),this.연습/ThisCall.java
@@ -0,0 +1,7 @@
+public class ThisCall {
+    public static void main(String[] args){
+        People noname = new People();
+        System.out.println("이름: "+ noname.name);
+        System.out.println("나이: "+noname.age);
+    }
+}
