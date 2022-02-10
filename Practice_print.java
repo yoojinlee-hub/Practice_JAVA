e87f48723677784241e4ed7fa3f3303f5d24f693
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-02-10 09:49:33 +0900

    Add files via upload

---
+++ Practice_print.java
@@ -0,0 +1,15 @@
+
+public class Practice_print {
+
+	public static void main(String[] args) {
+		int plus=40,minus=-20,multiply=300;
+		System.out.println("------------------------------");
+		System.out.print("µ¡¼À, »¬¼À, °ö¼ÀÀÇ");
+		System.out.println(" °á°ú¸¦ º¼±î¿ä?");
+		System.out.printf("a+b´Â %d, a-b´Â %d, a*b´Â %d",plus,minus,multiply);
+		System.out.println();
+		System.out.println("------------------------------");
+
+	}
+
+}
